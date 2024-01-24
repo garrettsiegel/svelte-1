138004b48fc1ad5e3a6bd442c3db20d2b385e25a
garrettsiegel <garrett@submurgedcreative.com>
2024-01-24 00:21:25 -0500

    Save progress pt1 Svelte docs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+  const name = 'Garrett'
+  const imgLink = 'https://res.cloudinary.com/drxro4uha/image/upload/v1698250312/gangstarr-mass-appeal_2_xijheg.jpg'
+</script>
+
+<h1>Hi {name.toUpperCase()}! Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<img src={imgLink} alt="" class="image">
+
+<style>
+  .image {
+    width: 100%;
+    max-width: 500px;
+  }
+</style>
