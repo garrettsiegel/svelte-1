010bfc6ee2e2a46e404833dbac009c34226a3656
garrettsiegel <garrett@submurgedcreative.com>
2024-01-24 22:40:06 -0500

    Svelte docs part1 - reactivity

---
+++ src/Nested.svelte
@@ -0,0 +1,43 @@
+<script>
+  const text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem cumque doloribus id <strong>exercitationem</strong> quam suscipit architecto alias odio? Aliquam, ipsum!`
+
+  let count = 0;
+  $: doubled = count * 2
+  $: {
+    console.log(`the count is ${count}`)
+    console.log(`the ${count} doubled is ${doubled}`)
+  }
+
+  $: if (count >= 10) {
+    console.log(`${count} is dangerously high!`)
+  }
+
+  function handleClick() {
+    count += 1
+  }
+
+  let numbers = [1, 2, 3, 4, 5]
+
+  function addNumber() {
+    numbers = [...numbers, numbers.length + 1]
+  }
+
+  $: sum = numbers.reduce((total, num) => total + num, 0)
+
+</script>
+
+<p>{@html text}</p>
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<p>{count} doubled is {doubled}</p>
+
+
+
+<p>{numbers.join(' + ')} = {sum}</p>
+<p>{numbers} - {numbers.length}</p>
+<button on:click={addNumber}>
+  Add a number
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
   const name = 'Garrett'
   const imgLink = 'https://res.cloudinary.com/drxro4uha/image/upload/v1698250312/gangstarr-mass-appeal_2_xijheg.jpg'
+  import Nested from '../Nested.svelte'
+  
 </script>
 
 <h1>Hi {name.toUpperCase()}! Welcome to SvelteKit</h1>
+<Nested />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <img src={imgLink} alt="" class="image">
@@ -12,5 +15,10 @@
   .image {
     width: 100%;
     max-width: 500px;
+  }
+
+  h1, p {
+    font-family: sans-serif;
+    font-size: 1.5rem;
   }
 </style>
