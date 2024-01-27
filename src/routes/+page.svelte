036616e34444fad38b99b2befd981bc242cd0899
garrettsiegel <garrett@submurgedcreative.com>
2024-01-26 21:52:38 -0500

    Update config for dealing with TS props errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,8 @@
 <script>
-  const name = 'Garrett'
-  const imgLink = 'https://res.cloudinary.com/drxro4uha/image/upload/v1698250312/gangstarr-mass-appeal_2_xijheg.jpg'
-  import Nested from '../Nested.svelte'
-  
+	import Nested from '../Nested.svelte';
 </script>
 
-<h1>Hi {name.toUpperCase()}! Welcome to SvelteKit</h1>
-<Nested />
+<Nested answer={42} />
+
+<h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<img src={imgLink} alt="" class="image">
-
-<style>
-  .image {
-    width: 100%;
-    max-width: 500px;
-  }
-
-  h1, p {
-    font-family: sans-serif;
-    font-size: 1.5rem;
-  }
-</style>
